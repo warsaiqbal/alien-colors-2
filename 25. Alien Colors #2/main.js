20a47e83713a9c6588eef9ca5eaa94ff0226ff23
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:21:03 -0700

    Add files via upload

---
+++ 25. Alien Colors #2/main.js
@@ -0,0 +1,16 @@
+//Runs if block
+var alien_color = "green";
+if (alien_color == 'green') {
+    console.log("You earned 5 points for shooting the alien!");
+}
+else {
+    console.log("10 points earned!");
+}
+//Runs else block
+var alien_color2 = "red";
+if (alien_color2 == 'green') {
+    console.log("You earned 5 points for shooting the alien!");
+}
+else {
+    console.log("10 points earned!");
+}
---
+++ 25. Alien Colors #2/main.ts
@@ -0,0 +1,15 @@
+//Runs if block
+let alien_color: string = "green"
+if (alien_color == 'green'){
+    console.log("You earned 5 points for shooting the alien!");
+} else {
+    console.log("10 points earned!");
+}
+
+//Runs else block
+let alien_color2: string = "red"
+if (alien_color2 == 'green'){
+    console.log("You earned 5 points for shooting the alien!");
+} else {
+    console.log("10 points earned!");
+}
